69150a198c1e506597b8af4d76ad780655506cdd
Barinder Banwait <b.s.banwait86@gmail.com>
2024-01-24 18:37:10 +0000

    add script to compute kappa consts

---
+++ granville_constants/kappa_consts.py
@@ -0,0 +1,101 @@
+"""kappa_consts.py
+
+    Quick scratch code to compute Granville's kappa' constant
+    for X_1(13), X_1(16), and X_1(18).
+
+    This just follows the definitions in Section 1.1 of Granville's paper.
+    Since all three models have leading coefficient 1, we can simplify the
+    definition of omega_prime.
+
+"""
+
+### Globals
+
+R.<x> = QQ[]
+
+# how many Good euler factors to take in the product
+NUM_GOOD_PRIMES = 5000  
+
+# for the bad primes, how many terms to take in the series. Making this 
+# too large kills the speed massively.
+SERIES_TERMS = 7
+
+# the constants N and m are fixed by Granville as follows
+N = 6
+m = 2
+
+def omega(f,r):
+    if is_prime(r):
+        f = f.change_ring(GF(r))
+        return len(f.roots())
+    output = 0
+    for i in range(r):
+        if f(i)%r == 0:
+            output += 1
+    return output
+
+def omega_prime(f, p, k):
+    assert f.leading_coefficient() == 1
+    return p^(k-1) * (p - 1) * omega(f,p^k)
+
+
+def kappa_f_prime(f, V_f_prime, A_f_Q):
+    """
+        Computes the kappa_f_prime constant in Section 1.1
+
+        f - the polynomial
+        V_f_prime - the area of the unit ball for F, computed in euclidean_contribution.ipynb
+        A_f_Q - the constant of the same name, which is just #Aut_Q(C)/2
+        
+        returns: kappa_f_prime
+
+    """
+
+    disc_f = f.discriminant()
+
+    bad_primes = ZZ(disc_f).prime_divisors()
+    good_primes = [p for p in primes_first_n(NUM_GOOD_PRIMES) if p not in bad_primes]
+
+    good_prime_contribution = 1
+    for p0 in good_primes:
+        p = RR(p0)
+        term = 1 + omega(f,p0)*(p-1)*(p^(2*m/N) - 1) / (p* (p^m - p^(2*m/N)))
+        good_prime_contribution *= term
+
+    # euclidean_contribution = 6.132666484981952  # see euclidean_contribution.ipynb
+
+    # 3.9325551449299807 is the one for X1(13)
+    # 3.517982630376745 is the one for X1(18)
+
+    bad_prime_contribution = 1
+    for p in bad_primes:
+
+        terms_to_sum = [ (omega_prime(f,p,i*m)/RR(p)^(2*i*m*(1 - 1/N))) for i in range(1,SERIES_TERMS + 1) ]
+        term = sum(terms_to_sum)
+        term = 1 + (1 - (1/RR(p)^(2*m/N)))*term
+        bad_prime_contribution *= term
+
+    finalans = (V_f_prime/A_f_Q) * good_prime_contribution * bad_prime_contribution 
+    return finalans
+
+
+def main():
+    f = x*(x^2+1)*(x^2+2*x-1)
+    V_f_prime = 6.132666484981952
+    A_f_Q = 2
+    kappa = kappa_f_prime(f, V_f_prime, A_f_Q)
+    print(f"Kappa for X1(16) is {kappa}")
+
+    f = x^6 - 2*x^5 + x^4 - 2*x^3 + 6*x^2 - 4*x + 1
+    V_f_prime = 3.9325551449299807
+    A_f_Q = 3
+    kappa = kappa_f_prime(f, V_f_prime, A_f_Q)
+    print(f"Kappa for X1(13) is {kappa}")
+
+    f = x^6 + 2*x^5 + 5*x^4 + 10*x^3 + 10*x^2 + 4*x + 1
+    V_f_prime = 3.517982630376745
+    A_f_Q = 3
+    kappa = kappa_f_prime(f, V_f_prime, A_f_Q)
+    print(f"Kappa for X1(18) is {kappa}")
+
+main()
---
+++ results_v2.py
@@ -0,0 +1,76 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import Counter
+
+
+def points_to_plot(d_list):
+    my_d_list = d_list.copy()
+    my_d_list = [abs(x) for x in my_d_list]
+    counts = dict(Counter(my_d_list))
+    my_d_list = [(k,v) for k,v in dict(counts).items()]
+    my_d_list.sort()
+    i = 0
+    output = []
+    for x,f in my_d_list:
+        i += f
+        output.append((x,i))
+    return [(0,0)]+output
+
+fig, ax = plt.subplots(1,2,figsize=(12, 8))
+
+ax[0].set_xlabel('B')
+ax[0].set_ylabel('T_B(N)')
+ax[1].set_xlabel('B')
+ax[1].set_ylabel('T_B(N)')
+
+
+G_13 = [17, 113, 193, 313, 481, 1153, 1417, 2257, 3769, 3961, 5449, 6217, 6641, 9881]
+G_18 = [33, 337, 457, 1009, 1993, 2833, 7369, 8241, 9049]
+
+G_16 = [ -8030, -8022, -7990, -7462, -6970, -6110, -5106, -3966, -3927, -3255, -3066, -2405, -2030, -1326, -1271, -671, -455, -290, -119, -15, 1, 
+10, 15, 41, 51, 70, 93, 105, 205, 217, 391, 546, 609, 679, 1105, 1326, 1378, 1785, 1830, 2370, 3145, 3289, 3585, 3705, 4505, 4522, 4745, 
+4945, 5565, 5865, 5890, 6355, 8169, 8570, 9334 ]
+
+points_to_plot_13 = points_to_plot(G_13)
+points_to_plot_18 = points_to_plot(G_18)
+points_to_plot_16 = points_to_plot(G_16)
+
+KAPPA_13 = 0.65
+KAPPA_18 = 0.4
+KAPPA_16 = 12.3490592865640
+
+x = np.linspace(0,10000,1000)
+
+y_13 = KAPPA_13 * np.cbrt(x)
+y_18 = KAPPA_18 * np.cbrt(x)
+y_16 = KAPPA_16 * np.cbrt(x)
+
+ax = plt.subplot(1, 2, 1)
+ax.set_title("N = 13 and 18")
+ax.set_xlabel('B')
+ax.set_ylabel('T_B(N)')
+
+
+plt.plot(*zip(*points_to_plot_13), 'or')
+plt.plot(*zip(*points_to_plot_13), 'r-', label="N=13")
+
+plt.plot(*zip(*points_to_plot_18), 'or')
+plt.plot(*zip(*points_to_plot_18), 'b-', label="N=18")
+
+plt.plot(x, y_13, 'g-', label="conjectural")
+plt.plot(x, y_18, 'g-')
+plt.legend(loc="upper left")
+
+ax = plt.subplot(1, 2, 2)
+ax.set_title("N = 16")
+ax.set_xlabel('B')
+ax.set_ylabel('T_B(16)')
+
+plt.plot(*zip(*points_to_plot_16), 'or')
+plt.plot(*zip(*points_to_plot_16), '-', color="orange", label="N=16")
+
+plt.plot(x, y_16, 'g-', label="conjectural")
+plt.legend(loc="lower right")
+
+plt.savefig('granville_distribution.png', dpi=400)
